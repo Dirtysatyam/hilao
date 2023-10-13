8c97c3099852a5afde860c351373546238b05769
SATYAM KUMAR <satyamx10@gmail.com>
2023-10-13 13:09:28 +0530

    laalla

---
+++ newData.js
@@ -0,0 +1,57 @@
+import { Client } from "porn-x";
+import fs from "fs";
+
+const client = new Client();
+
+const namesFilePath = "newNames.json";
+const names = JSON.parse(fs.readFileSync(namesFilePath, "utf-8"));
+
+const fetchDataForName = async (name) => {
+  try {
+    const information = await client.getInformation(name);
+    const gifResult = await client.getGif(name);
+    const imagesResult = await client.getImages(name);
+    const shortVideosResult = await client.getShortVideos(name);
+
+    return {
+      name,
+      information,
+      section1: gifResult.gifs,
+      section2: imagesResult.images,
+      section3: shortVideosResult,
+    };
+  } catch (error) {
+    console.error(`Error fetching data for ${name}:`, error);
+    return null;
+  }
+};
+
+const fetchDataForAllNames = async () => {
+  const allData = [];
+
+  for (const name of names) {
+    const data = await fetchDataForName(name);
+    if (data) {
+      allData.push(data);
+      console.log(name+' has been saved');
+    }
+  }
+
+  return allData;
+};
+
+const saveDataToFile = (data, filePath) => {
+  const dataToSave = JSON.stringify(data, null, 2);
+  fs.writeFileSync(filePath, dataToSave);
+  console.log(`Result saved to ${filePath}`);
+};
+
+const main = async () => {
+  const allData = await fetchDataForAllNames();
+
+  // Save all data to a single file
+  saveDataToFile(allData, "newData.json");
+};
+
+// Run the script
+main();
--- show.js
+++ show.js
@@ -1,26 +1,38 @@
-const headername= document.getElementById('headername')
+const headername = document.getElementById('headername');
+const searchForm = document.querySelector('form');
+const randomButton = document.querySelector('#random'); // Assuming the random button has an ID of 'random'
+let allUserData; // Store all user data globally
 
-async function fetchData() {
+async function fetchAllData() {
     try {
-        // Assuming 'allData.json' is in the same directory as your HTML file
         const response = await fetch('./allData.json');
-        const userData = await response.json();
+        allUserData = await response.json();
+    } catch (error) {
+        console.error("Error fetching all data:", error);
+        throw error;
+    }
+}
 
-        // Generate a random number between 1 and 100
-        const randomIndex = Math.floor(Math.random() * 33) + 1;
+function fetchData(name) {
+    try {
+        const user = allUserData.find(entry => entry.name.toLowerCase() === name.toLowerCase());
 
-        return userData[randomIndex - 1]; // Adjust for zero-based index
+        if (!user) {
+            throw new Error(`User with name ${name} not found`);
+        }
+
+        return user;
     } catch (error) {
         console.error("Error fetching data:", error);
-        throw error; // Propagate the error for better error handling
+        throw error;
     }
 }
 
 function updateBioSection(data) {
     const bioSection = document.getElementById("bio");
-    headername.innerText=data.name;
+    headername.innerText = data.name;
     bioSection.innerHTML = `
-    
+    <p><strong>Name:</strong> ${data.information.name}</p>
     <p><strong>Age:</strong> ${data.information.age}</p>
     <p><strong>boobsType:</strong> ${data.information.boobsType}</p>
     <p><strong>tattoos:</strong> ${data.information.tattoos}</p>
@@ -33,104 +45,105 @@ function updateBioSection(data) {
     <p><strong>hairColor:</strong> ${data.information.hairColor}</p>
     <p><strong>anotherNames:</strong> ${data.information.anotherNames}</p>
     <p><strong>About:</strong> ${data.information.about}</p>
-   
-`;}
+    `;
+}
 
 function updateSection1(data) {
     const section1 = document.getElementById("section1");
-
-    // Clear existing content in the container
     section1.innerHTML = '';
 
-    // Assuming data.section1 is an array of GIF URLs
     data.section1.forEach(gifUrl => {
-        // Create a card element
         const card = document.createElement('div');
-        card.classList.add('card'); // You might want to add some styling for the card
-
-        // Create an image element for the GIF
+        card.classList.add('card');
         const gifImage = document.createElement('img');
         gifImage.src = gifUrl;
-        gifImage.style.width = '400px'; // Set a fixed width
-        gifImage.style.height = '250px'; // Set a fixed height
-
-        // Append the image to the card
+        gifImage.style.width = '400px';
+        gifImage.style.height = '250px';
         card.appendChild(gifImage);
-
-        // Append the card to the container
         section1.appendChild(card);
     });
 }
 
-
 function updateSection2(data) {
     const section2 = document.getElementById("section2");
-
-    // Clear existing content in the container
     section2.innerHTML = '';
 
-    // Assuming data.section2 is an array of image URLs
     data.section2.forEach(imageUrl => {
-        // Create a card element
         const card = document.createElement('div');
-        card.classList.add('card'); // You might want to add some styling for the card
-
-        // Create an image element for the image
+        card.classList.add('card');
         const image = document.createElement('img');
         image.src = imageUrl;
-        image.style.width = '300px'; // Set a fixed width
-        image.style.height = '450px'; // Set a fixed height
-
-        // Append the image to the card
+        image.style.width = '300px';
+        image.style.height = '450px';
         card.appendChild(image);
-
-        // Append the card to the container
         section2.appendChild(card);
     });
 }
 
-
 function updateSection3(data) {
     const section3 = document.getElementById("section3");
-
-    // Clear existing content in the container
     section3.innerHTML = '';
 
-    // Assuming data.section3 is an array of video URLs
     data.section3.forEach(videoUrl => {
-        // Create a card element
         const card = document.createElement('div');
-        card.classList.add('card'); // You might want to add some styling for the card
-
-        // Create a video element
+        card.classList.add('card');
         const video = document.createElement('video');
         video.src = videoUrl;
         video.controls = true;
-        video.style.width = '350px'; // Set a fixed width for the video
-        video.style.height = '200px'; // Set a fixed height for the video
-
-        // Append the video to the card
+        video.style.width = '350px';
+        video.style.height = '200px';
         card.appendChild(video);
-
-        // Append the card to the container
         section3.appendChild(card);
     });
 }
 
-
-async function updateSections() {
-    try {
-        const userData = await fetchData();
-
-        // Call the update functions with the fetched data
-        updateBioSection(userData);
-        updateSection1(userData);
-        updateSection2(userData);
-        updateSection3(userData);
-    } catch (error) {
-        console.error("Error updating sections:", error);
-    }
+function updateSections(data) {
+    updateBioSection(data);
+    updateSection1(data);
+    updateSection2(data);
+    updateSection3(data);
 }
 
-// Call the function to initiate the update process
-updateSections();
+searchForm.addEventListener('submit', async function (event) {
+    event.preventDefault();
+
+    const searchTerm = event.target.elements.search.value;
+
+    try {
+        const userData = await fetchData(searchTerm);
+        updateSections(userData);
+        searchForm.reset();
+    } catch (error) {
+        console.error("Error updating sections:", error);
+        headername.innerText = "No Such Star";
+        const bioSection = document.getElementById("bio");
+        bioSection.innerHTML = '<h1>Make Sure u r writing the exact name. If u r sure then let us know ur star name , we will add her too.</h1>';
+        const section1 = document.getElementById("section1");
+        section1.innerHTML = '';
+        const section2 = document.getElementById("section2");
+        section2.innerHTML = '';
+        const section3 = document.getElementById("section3");
+        section3.innerHTML = '';
+    }
+});
+
+function getRandomUserData() {
+    const randomIndex = Math.floor(Math.random() * allUserData.length);
+    return allUserData[randomIndex];
+}
+
+randomButton.addEventListener('click', function () {
+    try {
+        const randomUserData = getRandomUserData();
+        updateSections(randomUserData);
+    } catch (error) {
+        console.error("Error updating sections with random data:", error);
+        headername.innerText = "Error";
+    }
+});
+
+fetchAllData().then(() => {
+    // Initial data fetch and update
+    const initialRandomUserData = getRandomUserData();
+    updateSections(initialRandomUserData);
+});
